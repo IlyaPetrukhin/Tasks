f9a008c526476da5f04b7c206621889ab7d25de2
Ilya Petruhin <59193488+IlyaPetruhin@users.noreply.github.com>
2020-04-24 15:04:57 +0300

    9Tasks

---
+++ Practice/Task10/script.js
@@ -0,0 +1,12 @@
+//(latitude, longitude)
+function getCoords(latitude, longitude) {
+    alert(`широта: ${latitude}, долгота: ${longitude}`);
+  }
+  let script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDmPoTVtQSdPG8JGWPp8nR3wa9Fqd_oJvU=places`;
+document.body.append(script);
+
+
+
+
+
--- Practice/Task9/script.js
+++ Practice/Task9/script.js
@@ -1,10 +1,10 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const numberButtons = document.querySelectorAll('[number]')
+const operationButtons = document.querySelectorAll('[operation]')
+const equalsButton = document.querySelector('[equals]')
+const deleteButton = document.querySelector('[delete]')
+const allClearButton = document.querySelector('[all-clear]')
+const previousOperandTextElement = document.querySelector('[previous-operand]')
+const currentOperandTextElement = document.querySelector('[current-operand]')
 
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
