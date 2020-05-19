b346fb4e614bdf2042f8126c700b35a29020ffa2
Ilya Petruhin <59193488+IlyaPetruhin@users.noreply.github.com>
2020-05-19 17:59:49 +0300

    10task

--- Practice/Task10/script.js
+++ Practice/Task10/script.js
@@ -1,26 +1,19 @@
-/*var input = document.getElementById('autocomplete');
-function initMap() {
-  var autocomplete = new google.maps.places.Autocomplete(input);
- 
-  info.addEventListener('click', function (){
-      let path = fetch('https://api.weatherbit.io/v2.0/forecast/daily?city='+autocomplete.value+'&key=16632115da314485b772f3d91c04d1aa')
-        .then(response=>response.json())
-        .then(result=>console.log(result))
-        return path
-      })
-}
-*/
+
 
 var info = document.getElementById('infowindow-content')
 let autocomplete = document.getElementById('autocomplete')
+let weater = document.getElementById('weater')
 function initMap() {
    autocomplete = new google.maps.places.Autocomplete(autocomplete)
    }
-   info.addEventListener('click',fetch('https://api.weatherbit.io/v2.0/forecast/daily?&key=16632115da314485b772f3d91c04d1aa&'+autocomplete.value+'')
+   info.addEventListener('click',function(){
+     
+     let path = fetch('http://api.weatherbit.io/v2.0/forecast/daily?city='+weater.value+'&key=16632115da314485b772f3d91c04d1aa')
     .then(response=>response.json())
     .then(function(init){
-      return console.log(init)}
-    ))
+      return console.log(init)})
+      return path
+    })
    
    //let path = fetch('https://api.weatherbit.io/v2.0/forecast/daily?&key=16632115da314485b772f3d91c04d1aa')
 
