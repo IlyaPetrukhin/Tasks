3d2208070bd81afecca60423f4c8e520a0003593
Ilya Petruhin <59193488+IlyaPetruhin@users.noreply.github.com>
2020-05-29 12:53:12 +0300

    Task 10

--- Practice/Task10/script.js
+++ Practice/Task10/script.js
@@ -1,21 +1,34 @@
 
 
 var info = document.getElementById('infowindow-content')
-let autocomplete = document.getElementById('autocomplete')
-let weater = document.getElementById('weater')
+let weather = document.getElementById('weather')
 function initMap() {
+  let autocomplete = document.getElementById('autocomplete')
    autocomplete = new google.maps.places.Autocomplete(autocomplete)
    }
-   info.addEventListener('click',function(){
+info.addEventListener('click',function(){
      
-     let path = fetch('http://api.weatherbit.io/v2.0/forecast/daily?city='+weater.value+'&key=16632115da314485b772f3d91c04d1aa')
+     let answer = fetch('http://api.weatherbit.io/v2.0/forecast/daily?city='+weather.value+'&key=16632115da314485b772f3d91c04d1aa')
     .then(response=>response.json())
     .then(function(init){
-      return console.log(init)})
-      return path
+      console.log(init.data)
+      const html = init.data.map(user=>{
+        return `<tr>
+        <th scope='row'>${user.valid_date}</th>
+        <td>${user.high_temp}</td>
+        <td>${user.weather.description}</td>
+        </tr> `
+      })
+      .join('')
+      document.querySelector('#app')
+              .insertAdjacentHTML('afterend',html)
+
+      })
+      .catch(error=>{
+        console.log(error)
+      })
     })
-   
-   //let path = fetch('https://api.weatherbit.io/v2.0/forecast/daily?&key=16632115da314485b772f3d91c04d1aa')
+
 
    
 
